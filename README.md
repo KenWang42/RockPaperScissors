7e41fba48246616af22132fcec6e179d59fd0579
KenWang42 <Kangyu.wang@gmail.com>
2019-09-17 16:41:16 +0800

    update: hover effeect

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RockPaperScissors
+
+Make a simple Rock Paper Scissors game with UI
+
+From The Odin Project's [curriculum](https://www.theodinproject.com/courses/web-development-101/lessons/rock-paper-scissors)
+
+[Github Page](https://kenwang42.github.io/RockPaperScissors/.)
--- script.js
+++ script.js
@@ -9,63 +9,64 @@ const bottom_div = document.getElementById("bottom");
 const btn_group = document.querySelectorAll("#player-section > .btn");
 
 function randomChoice(arr) {
-    return arr[Math.floor(Math.random() * arr.length)]
-  }
-
-function computerPlay() {
-    return randomChoice(['Rock', 'Paper', 'Scissors'])
-  }
-
-function playRound(playerSelection) {
-    var computerSelection = computerPlay();
-
-    if (playerSelection === computerSelection) // Tie
-    {
-        round_result_p.innerHTML = "Tie! Both of you played " + playerSelection;
-    }
-    else if ( // Player Win
-        (playerSelection === "Rock" && computerSelection === "Scissors") 
-    ||  (playerSelection === "Scissors" && computerSelection === "Paper")
-    ||  (playerSelection === "Paper" && computerSelection === "Rock"))
-    {    
-        playerScore += 1;
-        playerScore_span.innerHTML = playerScore;
-        round_result_p.innerHTML = "You Win! " + playerSelection + " beats " + computerSelection;
-    }
-    else // Player Lose
-    {
-        computerScore += 1;
-        computerScore_span.innerHTML = computerScore;
-        round_result_p.innerHTML = "You Lose! " + computerSelection + " beats " + playerSelection;
-    }
-
-    roundNumber += 1;
-    round_number_span.innerHTML = roundNumber;
-
-    if (computerScore >= 5){
-        round_result_p.innerHTML = "You Lose the Game!";
-        endGame();
-    }
-
-    if (playerScore >= 5){
-        round_result_p.innerHTML = "You Win the Game!";
-        endGame();
-    }
-  }
-
-function endGame(){
-    btn_group.forEach(btn => btn.disabled = true);
-    bottom_div.classList.remove("hidden");
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function resetGame(){
-    playerScore = 0;
-    computerScore = 0;
-    roundNumber = 0;
-    round_number_span.innerHTML = 0;
-    playerScore_span.innerHTML = 0;
-    computerScore_span.innerHTML = 0;
-    round_result_p.innerHTML = "Choose Your Move";
-    bottom_div.classList.add("hidden");
-    btn_group.forEach(btn => btn.disabled = false);
+function computerPlay() {
+  return randomChoice(["Rock", "Paper", "Scissors"]);
+}
+
+function playRound(playerSelection) {
+  var computerSelection = computerPlay();
+
+  if (playerSelection === computerSelection) {
+    // Tie
+    round_result_p.innerHTML = "Tie! Both of you played " + playerSelection;
+  } else if (
+    // Player Win
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Rock")
+  ) {
+    playerScore += 1;
+    playerScore_span.innerHTML = playerScore;
+    round_result_p.innerHTML =
+      "You Win! " + playerSelection + " beats " + computerSelection;
+  } // Player Lose
+  else {
+    computerScore += 1;
+    computerScore_span.innerHTML = computerScore;
+    round_result_p.innerHTML =
+      "You Lose! " + computerSelection + " beats " + playerSelection;
+  }
+
+  roundNumber += 1;
+  round_number_span.innerHTML = roundNumber;
+
+  if (computerScore >= 5) {
+    round_result_p.innerHTML = "You Lose the Game!";
+    endGame();
+  }
+
+  if (playerScore >= 5) {
+    round_result_p.innerHTML = "You Win the Game!";
+    endGame();
+  }
+}
+
+function endGame() {
+  btn_group.forEach(btn => (btn.disabled = true));
+  bottom_div.classList.remove("hidden");
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  roundNumber = 0;
+  round_number_span.innerHTML = 0;
+  playerScore_span.innerHTML = 0;
+  computerScore_span.innerHTML = 0;
+  round_result_p.innerHTML = "Choose Your Move";
+  bottom_div.classList.add("hidden");
+  btn_group.forEach(btn => (btn.disabled = false));
 }
